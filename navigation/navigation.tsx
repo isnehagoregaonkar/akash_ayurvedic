c622b38d88750051e885d3b0dcabe99bd4bdd881
Snehal Goregaonkar <goregaonkarsneha98@gmail.com>
2023-07-06 00:16:39 +0530

    google plist added and few dependencies installed

---
+++ navigation/navigation.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const navigation = () => {
+  return (
+    <View>
+      <Text>navigation</Text>
+    </View>
+  )
+}
+
+export default navigation
+
+const styles = StyleSheet.create({})
