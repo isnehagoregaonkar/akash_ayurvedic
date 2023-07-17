5d171537d3b9dc49a5901bdd93506ef09ba5a120
Snehal Goregaonkar <goregaonkarsneha98@gmail.com>
2023-07-17 15:16:09 +0530

    firebase setup

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,19 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
-        'react-native-reanimated/plugin', {
-            relativeSourceLocation: true,
-        },
-    ]
-],
+      'react-native-reanimated/plugin', {
+        relativeSourceLocation: true,
+      },
+    ],
+    ["module:react-native-dotenv", {
+      "envName": "APP_ENV",
+      "moduleName": "@env",
+      "path": ".env",
+      "safe": false,
+      "allowUndefined": true,
+      "verbose": false
+    }]
+  ],
   env: {
     production: {
       plugins: ['react-native-paper/babel'],
---
+++ utils/firebase.ts
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: process.env['API_KEY'],
+  authDomain: process.env['AUTH_DOMAIN'],
+  projectId: process.env['PROJECT_ID'],
+  storageBucket: process.env['STORAGE_BUCKET'],
+  messagingSenderId:process.env['MESSAING_SENDER_ID'],
+  appId: process.env['APP_ID'],
+  measurementId: "G-3FWZ5983ZC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
